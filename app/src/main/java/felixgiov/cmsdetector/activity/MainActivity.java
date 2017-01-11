0c304b9bfa5824d0149a22cf358f9acf3b111ec7
Felix Giovanni <gv.felix@yahoo.co.id>
2017-01-11 21:52:02 +0700

    Firebase Ads/AdMob has been added. Major changes in UI.

--- app/src/main/java/felixgiov/cmsdetector/activity/MainActivity.java
+++ app/src/main/java/felixgiov/cmsdetector/activity/MainActivity.java
@@ -5,8 +5,16 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.util.List;
 
@@ -21,32 +29,63 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private final static String API_KEY = "baf1270d5a9c8f3dcf4de5c11f4ca392";
+    private AdView mAdView;
+    ImageButton klik;
+    EditText url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        klik = (ImageButton) findViewById(R.id.klik_button);
+        url = (EditText) findViewById(R.id.url_et);
+
         if (API_KEY.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Please obtain your API KEY first!", Toast.LENGTH_LONG).show();
             return;
         }
 
+        klik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rest(url.getText().toString());
+            }
+        });
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, "ca-app-pub-2029021861713537/7143027207");
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        mAdView = (AdView) findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
+
+    }
+    public void rest(String url){
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
-        Call<List<String>> call = apiService.detectCMS("ugm.ac.id",API_KEY);
+        Call<List<String>> call = apiService.detectCMS(url,API_KEY);
         call.enqueue(new Callback<List<String>>() {
             @Override
             public void onResponse(Call<List<String>> call, Response<List<String>> response) {
                 if (response.isSuccessful()) {
                     List<String> cms = response.body();
                     recyclerView.setAdapter(new CmsAdapter(cms, R.layout.list_tab, getApplicationContext()));
-                        Log.d("list", cms.toString());
+                    Log.d("list", cms.toString());
 
                 } else {
                     // error response, no access to resource?
